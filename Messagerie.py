fc4a4b6373939f65dad24fad81b515ae35946560
= <=>
2026-01-31 21:20:51 -0500

    Messagerie

--- Messagerie.py
+++ Messagerie.py
@@ -6,4 +6,22 @@ afin de suivre leur progression et les événements importants.
 
 class Messagerie:
 
+    def __init__(self):
+        self.messages = []
+
+    def envoyer(self, message):
+        self.archiver(message)
+        self.afficher(message)
+
+    def archiver(self, message):
+        self.messages.append(message)
+
+    def afficher_archive(self):
+        for msg in self.messages:
+            print(msg)
+
+    def afficher(self, message):
+        print(message)
+
+
     
