dac09e25efa0169ff4feaca73533de74987fcd79
Malek <202275145@edu.clg.qc.ca>
2026-02-04 17:20:39 -0500

    Fix Coureur

--- Coureur.py
+++ Coureur.py
@@ -5,24 +5,26 @@
 
 import random
 import time
+import Piste
 
 class Coureur:
     VITESSE_MIN = 1
     VITESSE_MAX = 3
     #Philip: Joueur devra avoir temps total.Je dirai pas de distance parcouru mais en combien de temps car c'est la meme piste
 
-    def __init__(self, id: int, couleur: str, position = 0, vitesse = 1):
+    def __init__(self, id: int, couleur: str, piste: Piste, position = 0, vitesse = 1):
         self.id = id # Id du joueur
         self.couleur = couleur # Couleur du coureur(IDENTIFICATION)
         self.position = position # Position dans la course
         self.vitesse = vitesse  # Vitesse en secondes par case
+        self.piste = piste # Piste du coureur(de la classe Piste)
         self.temps_total = None # Temps du coureur du début jusqu'à la fin de la course
         # Le temps sera instancier au moment du départ de la course, par time_perf_counter()
 
 
     
     # Méthode qui retourne le nombre de secondes que prend un coureur à changer de case
-    def avancer(self, vitesse):
+    def avancer(self, vitesse) -> int:
         vitesse = Coureur.modifier_vitesse()
 
         return vitesse
